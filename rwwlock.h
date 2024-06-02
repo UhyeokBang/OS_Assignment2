5db1d54f6646fe67ea8f969ef043dd23bfc2a108
Bang Uhyeok <0105bang@gmail.com>
2024-06-02 18:33:46 +0900

    made lock

--- rwwlock.h
+++ rwwlock.h
@@ -1,18 +1,65 @@
-typedef struct _rwwlock_t {
+#ifndef RWWLOCK_H
+#define RWWLOCK_H
+
+#include <semaphore.h>
+
+typedef struct _rwwlock_t
+{
+    sem_t read_lock;  // Semaphore to control access to reader count
+    sem_t write_lock; // Semaphore to control access to the critical section
+    sem_t entry_lock; // Semaphore to prevent new readers when a writer is waiting
+    int readers;      // Number of readers currently accessing the resource
 } rwwlock_t;
+
 void rwwlock_init(rwwlock_t *rww);
 void rwwlock_acquire_writelock(rwwlock_t *rww);
 void rwwlock_release_writelock(rwwlock_t *rww);
 void rwwlock_acquire_readlock(rwwlock_t *rww);
 void rwwlock_release_readlock(rwwlock_t *rww);
 
-void rwwlock_init(rwwlock_t *rww){
+void rwwlock_init(rwwlock_t *rww)
+{
+    sem_init(&rww->read_lock, 0, 1);  // Binary semaphore
+    sem_init(&rww->write_lock, 0, 1); // Binary semaphore
+    sem_init(&rww->entry_lock, 0, 1); // Binary semaphore
+    rww->readers = 0;
 }
-void rwwlock_acquire_writelock(rwwlock_t *rww){
+
+void rwwlock_acquire_writelock(rwwlock_t *rww)
+{
+    sem_wait(&rww->entry_lock); // Prevent new readers
+    sem_wait(&rww->write_lock); // Acquire write lock
 }
-void rwwlock_release_writelock(rwwlock_t *rww){
+
+void rwwlock_release_writelock(rwwlock_t *rww)
+{
+    sem_post(&rww->write_lock); // Release write lock
+    sem_post(&rww->entry_lock); // Allow new readers
 }
-void rwwlock_acquire_readlock(rwwlock_t *rww){
+
+void rwwlock_acquire_readlock(rwwlock_t *rww)
+{
+    sem_wait(&rww->entry_lock); // Ensure no new writers
+    sem_post(&rww->entry_lock); // Allow other readers to enter
+
+    sem_wait(&rww->read_lock); // Lock to update readers count
+    rww->readers++;
+    if (rww->readers == 1)
+    {
+        sem_wait(&rww->write_lock); // First reader locks the write lock
+    }
+    sem_post(&rww->read_lock); // Unlock after updating readers count
 }
-void rwwlock_release_readlock(rwwlock_t *rww){
+
+void rwwlock_release_readlock(rwwlock_t *rww)
+{
+    sem_wait(&rww->read_lock); // Lock to update readers count
+    rww->readers--;
+    if (rww->readers == 0)
+    {
+        sem_post(&rww->write_lock); // Last reader unlocks the write lock
+    }
+    sem_post(&rww->read_lock); // Unlock after updating readers count
 }
+
+#endif // RWWLOCK_H
--- seqlock.h
+++ seqlock.h
@@ -1,23 +1,49 @@
-typedef struct _seqlock_t {
+#ifndef SEQLOCK_H
+#define SEQLOCK_H
+
+#include <semaphore.h>
+#include <stdatomic.h>
+
+typedef struct _seqlock_t
+{
+    atomic_uint counter; // Atomic counter for sequence number
+    sem_t write_lock;    // Semaphore for write lock
 } seqlock_t;
+
 void seqlock_init(seqlock_t *seq);
 void seqlock_write_lock(seqlock_t *seq);
 void seqlock_write_unlock(seqlock_t *seq);
 unsigned seqlock_read_begin(seqlock_t *seq);
 unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt);
 
-void seqlock_init(seqlock_t *seq){
-
+void seqlock_init(seqlock_t *seq)
+{
+    atomic_init(&seq->counter, 0);    // Initialize counter to 0
+    sem_init(&seq->write_lock, 0, 1); // Initialize write lock
 }
-void seqlock_write_lock(seqlock_t *seq){
 
+void seqlock_write_lock(seqlock_t *seq)
+{
+    sem_wait(&seq->write_lock);         // Acquire the write lock
+    atomic_fetch_add(&seq->counter, 1); // Increment the counter (make it odd)
 }
-void seqlock_write_unlock(seqlock_t *seq){
 
+void seqlock_write_unlock(seqlock_t *seq)
+{
+    atomic_fetch_add(&seq->counter, 1); // Increment the counter (make it even)
+    sem_post(&seq->write_lock);         // Release the write lock
 }
-unsigned seqlock_read_begin(seqlock_t *seq){
 
+unsigned seqlock_read_begin(seqlock_t *seq)
+{
+    return atomic_load(&seq->counter); // Read the counter
 }
-unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt){
 
+unsigned seqlock_read_retry(seqlock_t *seq, unsigned cnt)
+{
+    unsigned current = atomic_load(&seq->counter); // Read the counter again
+    // Retry if the counter is different or odd
+    return (current != cnt || (current & 1) != 0);
 }
+
+#endif // SEQLOCK_H
